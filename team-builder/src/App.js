57ca59bebce95da5f42f33d067162f6ea3452320
Jevon Cochran <jevon@Jevons-MacBook-Pro.local>
2019-11-14 21:08:13 -0300

    got input rendering to page, hit MVP

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,34 +1,40 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
+import TeamForm from "./components/TeamForm";
+import TeamMembers from "./components/TeamMembers";
 import './App.css';
 
 function App() {
-  const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    role: '',
-  });
+  const [teamMembers, setTeamMembers] = useState([
+    {
+      firstName: 'Jevon',
+      lastName: 'Cochran',
+      email: 'jevon.cochran@gmail.com',
+      role: 'Web Developer',
+    },
+    {
+      firstName: 'Kianna',
+      lastName: 'Pinkney',
+      email: 'k.pinkney90@gmail.com',
+      role: 'IT',
+    }
+  ]);
 
-  const handleChanges = event => {
-    setFormData({
-      ...formData,
-      [event.target.id]: event.target.value
-    });
-  };
+  const addIndividual = individual => {
+    const newIndividual = {
+      id: Date.now(),
+      firstName: individual.firstName,
+      lastName: individual.lastName,
+      email: individual.email,
+      role: individual.role
+    }
+    setTeamMembers([...teamMembers, newIndividual]);
+  }
 
   return (
     <div className="App">
-      <h1>Add Team Member</h1>
-      <form>
-        <label>First Name: <input type="text" id="firstName" onChange={handleChanges} /></label><br />
-        <label>Last Name: <input type="text" id="lastName" onChange={handleChanges}/></label><br />
-        <label>Email:<input type="email" id="email" onChange={handleChanges} /></label><br />
-        <label>Role: <input type="text" id="role" onChange={handleChanges} /></label><br /> 
-        <input type="submit" />
-      </form><br /><br /><br />
-      <h2>Team Members</h2>
-      <h3>{formData.lastName}, {formData.firstName}</h3>
+      <h1>Team Members</h1>
+      <TeamForm addIndividual={addIndividual} />
+      <TeamMembers teamMembers={teamMembers} />
     </div>
   );
 }
